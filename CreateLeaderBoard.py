8fde0ddb14dbe431369f0da280230edd03b3252d
larsver <lars.larsvermeulen@gmail.com>
2021-07-24 17:50:01 +0200

    HTML toevoeging

--- CreateLeaderBoard.py
+++ CreateLeaderBoard.py
@@ -1,3 +1,19 @@
+#!/Python27/python
+print "Content-Type: text/html\n\n";
+print """<html>
+<head>
+<title>Untappd Leaderboard</title>
+<style>
+  table, th, td {
+  border: 1px solid black;
+  text-align: center;
+  border-collapse: collapse;
+}
+</style>
+</head>
+<body>
+"""
+
 import re
 import requests
 import time
@@ -8,10 +24,6 @@ from requests.packages.urllib3.exceptions import InsecureRequestWarning
 ###########################
 # Functions
 ###########################
-
-
-
-
 
 def get_data_from_untappd(url):
     # Setting up and Making the Web Call
@@ -43,19 +55,37 @@ def print_leaderboard(title, user_data_list):
         array_index = 1
     elif (title == 'Total Badges'):
         array_index = 3
+    print "<h4>"
     print(title)
+    print """</h4>
+        <table>
+        <tr>
+            <th>Ranking</th>
+            <th>Naam</th>
+            <th>Aantal</th>
+        </tr>
+        """
     counter = 1
     rows = []
     for user_data in user_data_list:
-        leaderboard_entry = ['    ' + str(counter) + ')', find_real_name(user_data[0]), str(user_data[array_index])]
-        rows.append(leaderboard_entry)
+        print "<tr><td>"
+        print str(counter) + ') </td>'
+        print "<td>" + find_real_name(user_data[0]) + "</td>"
+        print "<td>" + str(user_data[array_index]) + "</td>"
+        print "</tr>"
+        
+        #leaderboard_entry = ['    ' + str(counter) + ')', find_real_name(user_data[0]), str(user_data[array_index])]
+        #rows.append(leaderboard_entry)
         counter += 1
-    lens = []
-    for col in zip(*rows):
-        lens.append(max([len(v) for v in col]))
-    format = "  ".join(["{:<" + str(l) + "}" for l in lens])
-    for row in rows:
-        print(format.format(*row))
+        
+    print "</table>"
+    
+    #lens = []
+    #for col in zip(*rows):
+        #lens.append(max([len(v) for v in col]))
+    #format = "  ".join(["{:<" + str(l) + "}" for l in lens])
+    #for row in rows:
+        #print(format.format(*row))
 
 def find_real_name(nickname):
     if (nickname == 'eurniee'):
@@ -68,26 +98,37 @@ def find_real_name(nickname):
 ###########################
 # Start
 ###########################
+def main() : 
+    
+    
+    # Suppress HTTPS warnings
+    requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
-# Suppress HTTPS warnings
-requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+    # Get user info
+    # user_list = ['eurniee', 'BassieWouters', 'Den_Henry']
+    user_list = ['eurniee', 'BassieWouters']
+    user_data_list = []
 
-# Get user info
-user_list = ['eurniee', 'BassieWouters', 'Den_Henry']
-user_data_list = []
+    for user in user_list:   
+        user_data = get_user_data(user)
+        user_data_list.append([user, int(format(user_data[0].text)), int(format(user_data[1].text)), int(format(user_data[2].text))])
+    
+    # Create leaderboard 'Total Unique'
+    leaderboard_total_unique = sorted(user_data_list, key = lambda x: x[2], reverse = True)
+    print_leaderboard('Total Unique', leaderboard_total_unique)
+   
+    # Create leaderboard 'Total Beers'
+    leaderboard_total_beers = sorted(user_data_list, key = lambda x: x[1], reverse = True)
+    print_leaderboard('Total Beers', leaderboard_total_beers)
+    
+    # Create leaderboard 'Total Badges'
+    leaderboard_total_badges = sorted(user_data_list, key = lambda x: x[3], reverse = True)
+    print_leaderboard('Total Badges', leaderboard_total_badges)
 
-for user in user_list:
-    user_data = get_user_data(user)
-    user_data_list.append([user, int(format(user_data[0].text)), int(format(user_data[1].text)), int(format(user_data[2].text))])
 
-# Create leaderboard 'Total Unique'
-leaderboard_total_unique = sorted(user_data_list, key = lambda x: x[2], reverse = True)
-print_leaderboard('Total Unique', leaderboard_total_unique)
+main()
+print """
+</body>
+</html>
+ """
 
-# Create leaderboard 'Total Beers'
-leaderboard_total_beers = sorted(user_data_list, key = lambda x: x[1], reverse = True)
-print_leaderboard('Total Beers', leaderboard_total_beers)
-
-# Create leaderboard 'Total Badges'
-leaderboard_total_badges = sorted(user_data_list, key = lambda x: x[3], reverse = True)
-print_leaderboard('Total Badges', leaderboard_total_badges)
