212731e9ce3ea37a8a2eee8aca82ce7d7f9b9c57
Arne Vermeulen <vermeulen.arne@gmail.com>
2022-01-01 17:23:16 +0100

    add leaderboard for 2021 and add new users

--- front-end/browser_main.js
+++ front-end/browser_main.js
@@ -1,15 +1,53 @@
 const total_unique_beers_button = document.getElementById('total_unique_beers_button');
 const total_beers_button = document.getElementById('total_beers_button');
 const total_badges_button = document.getElementById('total_badges_button');
+const button_2021 = document.getElementById('button_2021');
 
 total_unique_beers_button.onclick = async() => { showTab(total_unique_beers_button, 'total_unique_beers_tab'); };
 total_beers_button.onclick = async() => { showTab(total_beers_button, 'total_beers_tab'); };
 total_badges_button.onclick = async() => { showTab(total_badges_button, 'total_badges_tab'); };
+button_2021.onclick = async() => { showTab(button_2021, 'tab_2021'); };
 
 showTab(total_unique_beers_button, 'total_unique_beers_tab');
 start();
 
 async function start() {
+	let button_2012 = document.getElementById('button_2021');
+	button_2021.disabled = true;
+	button_2012.style.display = 'none';
+
+	let leaderboard2021 = await get('http://arne.ulyssis.be/Untappd/leaderboard2021.json');
+	if (leaderboard2021 != "") {
+		button_2021.disabled = false;
+		button_2012.style.display = 'block';
+
+		document.getElementById('total_unique_beers_position_1_name_2021').innerHTML = leaderboard2021.number_unique_beers.ranking.position_1;
+		document.getElementById('total_unique_beers_position_2_name_2021').innerHTML = leaderboard2021.number_unique_beers.ranking.position_2;
+		document.getElementById('total_unique_beers_position_3_name_2021').innerHTML = leaderboard2021.number_unique_beers.ranking.position_3;
+		document.getElementById('total_unique_beers_position_4_name_2021').innerHTML = leaderboard2021.number_unique_beers.ranking.position_4;
+		document.getElementById('total_unique_beers_position_5_name_2021').innerHTML = leaderboard2021.number_unique_beers.ranking.position_5;
+		document.getElementById('total_unique_beers_position_6_name_2021').innerHTML = leaderboard2021.number_unique_beers.ranking.position_6;
+		document.getElementById('total_unique_beers_position_7_name_2021').innerHTML = leaderboard2021.number_unique_beers.ranking.position_7;
+		document.getElementById('total_unique_beers_position_8_name_2021').innerHTML = leaderboard2021.number_unique_beers.ranking.position_8;
+		document.getElementById('total_unique_beers_position_9_name_2021').innerHTML = leaderboard2021.number_unique_beers.ranking.position_9;
+		document.getElementById('total_unique_beers_position_10_name_2021').innerHTML = leaderboard2021.number_unique_beers.ranking.position_10;
+		document.getElementById('total_unique_beers_position_11_name_2021').innerHTML = leaderboard2021.number_unique_beers.ranking.position_11;
+		document.getElementById('total_unique_beers_position_12_name_2021').innerHTML = leaderboard2021.number_unique_beers.ranking.position_12;
+
+		document.getElementById('total_unique_beers_position_1_points_2021').innerHTML = leaderboard2021.number_unique_beers.points.points_1;
+		document.getElementById('total_unique_beers_position_2_points_2021').innerHTML = leaderboard2021.number_unique_beers.points.points_2;
+		document.getElementById('total_unique_beers_position_3_points_2021').innerHTML = leaderboard2021.number_unique_beers.points.points_3;
+		document.getElementById('total_unique_beers_position_4_points_2021').innerHTML = leaderboard2021.number_unique_beers.points.points_4;
+		document.getElementById('total_unique_beers_position_5_points_2021').innerHTML = leaderboard2021.number_unique_beers.points.points_5;
+		document.getElementById('total_unique_beers_position_6_points_2021').innerHTML = leaderboard2021.number_unique_beers.points.points_6;
+		document.getElementById('total_unique_beers_position_7_points_2021').innerHTML = leaderboard2021.number_unique_beers.points.points_7;
+		document.getElementById('total_unique_beers_position_8_points_2021').innerHTML = leaderboard2021.number_unique_beers.points.points_8;
+		document.getElementById('total_unique_beers_position_9_points_2021').innerHTML = leaderboard2021.number_unique_beers.points.points_9;
+		document.getElementById('total_unique_beers_position_10_points_2021').innerHTML = leaderboard2021.number_unique_beers.points.points_10;
+		document.getElementById('total_unique_beers_position_11_points_2021').innerHTML = leaderboard2021.number_unique_beers.points.points_11;
+		document.getElementById('total_unique_beers_position_12_points_2021').innerHTML = leaderboard2021.number_unique_beers.points.points_12;
+	}
+
 	let leaderboard = await get('http://arne.ulyssis.be/Untappd/leaderboard.json');
 
 	document.getElementById('total_unique_beers_position_1_name').innerHTML = leaderboard.number_unique_beers.ranking.position_1;
@@ -24,6 +62,8 @@ async function start() {
 	document.getElementById('total_unique_beers_position_10_name').innerHTML = leaderboard.number_unique_beers.ranking.position_10;
 	document.getElementById('total_unique_beers_position_11_name').innerHTML = leaderboard.number_unique_beers.ranking.position_11;
 	document.getElementById('total_unique_beers_position_12_name').innerHTML = leaderboard.number_unique_beers.ranking.position_12;
+	document.getElementById('total_unique_beers_position_13_name').innerHTML = leaderboard.number_unique_beers.ranking.position_13;
+	document.getElementById('total_unique_beers_position_14_name').innerHTML = leaderboard.number_unique_beers.ranking.position_14;
     
 	document.getElementById('total_unique_beers_position_1_points').innerHTML = leaderboard.number_unique_beers.points.points_1;
 	document.getElementById('total_unique_beers_position_2_points').innerHTML = leaderboard.number_unique_beers.points.points_2;
@@ -37,6 +77,8 @@ async function start() {
 	document.getElementById('total_unique_beers_position_10_points').innerHTML = leaderboard.number_unique_beers.points.points_10;
 	document.getElementById('total_unique_beers_position_11_points').innerHTML = leaderboard.number_unique_beers.points.points_11;
 	document.getElementById('total_unique_beers_position_12_points').innerHTML = leaderboard.number_unique_beers.points.points_12;
+	document.getElementById('total_unique_beers_position_13_points').innerHTML = leaderboard.number_unique_beers.points.points_13;
+	document.getElementById('total_unique_beers_position_14_points').innerHTML = leaderboard.number_unique_beers.points.points_14;
     
 	document.getElementById('total_beers_position_1_name').innerHTML = leaderboard.number_total_beers.ranking.position_1;
 	document.getElementById('total_beers_position_2_name').innerHTML = leaderboard.number_total_beers.ranking.position_2;
@@ -50,6 +92,8 @@ async function start() {
 	document.getElementById('total_beers_position_10_name').innerHTML = leaderboard.number_total_beers.ranking.position_10;
 	document.getElementById('total_beers_position_11_name').innerHTML = leaderboard.number_total_beers.ranking.position_11;
 	document.getElementById('total_beers_position_12_name').innerHTML = leaderboard.number_total_beers.ranking.position_12;
+	document.getElementById('total_beers_position_13_name').innerHTML = leaderboard.number_total_beers.ranking.position_13;
+	document.getElementById('total_beers_position_14_name').innerHTML = leaderboard.number_total_beers.ranking.position_14;
     
 	document.getElementById('total_beers_position_1_points').innerHTML = leaderboard.number_total_beers.points.points_1;
 	document.getElementById('total_beers_position_2_points').innerHTML = leaderboard.number_total_beers.points.points_2;
@@ -63,6 +107,8 @@ async function start() {
 	document.getElementById('total_beers_position_10_points').innerHTML = leaderboard.number_total_beers.points.points_10;
 	document.getElementById('total_beers_position_11_points').innerHTML = leaderboard.number_total_beers.points.points_11;
 	document.getElementById('total_beers_position_12_points').innerHTML = leaderboard.number_total_beers.points.points_12;
+	document.getElementById('total_beers_position_13_points').innerHTML = leaderboard.number_total_beers.points.points_13;
+	document.getElementById('total_beers_position_14_points').innerHTML = leaderboard.number_total_beers.points.points_14;
     
 	document.getElementById('total_badges_position_1_name').innerHTML = leaderboard.number_badges.ranking.position_1;
 	document.getElementById('total_badges_position_2_name').innerHTML = leaderboard.number_badges.ranking.position_2;
@@ -76,6 +122,8 @@ async function start() {
 	document.getElementById('total_badges_position_10_name').innerHTML = leaderboard.number_badges.ranking.position_10;
 	document.getElementById('total_badges_position_11_name').innerHTML = leaderboard.number_badges.ranking.position_11;
 	document.getElementById('total_badges_position_12_name').innerHTML = leaderboard.number_badges.ranking.position_12;
+	document.getElementById('total_badges_position_13_name').innerHTML = leaderboard.number_badges.ranking.position_13;
+	document.getElementById('total_badges_position_14_name').innerHTML = leaderboard.number_badges.ranking.position_14;
     
 	document.getElementById('total_badges_position_1_points').innerHTML = leaderboard.number_badges.points.points_1;
 	document.getElementById('total_badges_position_2_points').innerHTML = leaderboard.number_badges.points.points_2;
@@ -89,6 +137,8 @@ async function start() {
 	document.getElementById('total_badges_position_10_points').innerHTML = leaderboard.number_badges.points.points_10;
 	document.getElementById('total_badges_position_11_points').innerHTML = leaderboard.number_badges.points.points_11;
 	document.getElementById('total_badges_position_12_points').innerHTML = leaderboard.number_badges.points.points_12;
+	document.getElementById('total_badges_position_13_points').innerHTML = leaderboard.number_badges.points.points_13;
+	document.getElementById('total_badges_position_14_points').innerHTML = leaderboard.number_badges.points.points_14;
 }
 
 function get(url) {
@@ -96,13 +146,17 @@ function get(url) {
 		var xhttp = new XMLHttpRequest();
 		xhttp.onreadystatechange = function() {			
 			if (this.readyState == 4 && this.status == 200) {
-				resolve(JSON.parse(this.responseText));
+				if (this.responseText.length > 0) {
+					resolve(JSON.parse(this.responseText));
+				}
+				else {
+					resolve(this.responseText);
+				}
 			}
 		};
 		xhttp.open('GET', url, true);
 		xhttp.send();
 	});
-
 	return promise;
 }
 
@@ -126,4 +180,15 @@ function showTab(button, tab) {
 	const currentTab = document.getElementById(tab);
 	currentTab.style.display = 'block';
 	button.className += ' active';
+}
+
+function findSize(fileInput) {
+    try{
+        return fileInput.files[0].size; // Size returned in bytes.
+    }catch(e){
+        var objFSO = new ActiveXObject("Scripting.FileSystemObject");
+        var e = objFSO.getFile( fileInput.value);
+        var fileSize = e.size;
+        return fileSize;    
+    }
 }
